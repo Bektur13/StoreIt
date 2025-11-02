b4c2e7ace5cb35f7e36751930d51296b4347083e
Bektur13 <duyshembekov04@mail.ru>
2025-11-01 23:27:16 -0700

    Configure all css variables.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className='flex min-h-screen'>{children}</div>
+  )
+}
+
+export default Layout
