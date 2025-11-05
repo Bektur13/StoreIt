01dc34d33e4d2f195881184fa17183738636023c
Bektur13 <duyshembekov04@mail.ru>
2025-11-04 19:34:26 -0800

    Update sessionClient

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,8 +1,23 @@
-import { Client } from 'node-appwrite';
+import { Account, Client, Databases } from 'node-appwrite';
 import { appwriteConfig } from './config';
+import { cookies } from 'next/headers';
 
 const createSessionClient = async () => {
-const client = new Client()
-.setEndpoint(appwriteConfig.endPointUrl)
-.setProject(appwriteConfig.projectId);
+    const client = new Client()
+            .setEndpoint(appwriteConfig.endPointUrl)
+            .setProject(appwriteConfig.projectId);
+    const session = (await cookies()).get('appwrite-session');
+
+    if(!session || !session.value) throw new Error('No session!');
+
+    client.setSession(session.value);
+
+    return {
+        get account() {
+            return new Account(client);
+        },
+        get database() {
+            return new Databases(client);
+        }
+    }
 }
