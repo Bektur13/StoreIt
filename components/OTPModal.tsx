6f35692523ef0dbefd43090e9fc98ac9bceb1f7b
Bektur13 <duyshembekov04@mail.ru>
2025-11-07 17:40:23 -0800

    Update OTP component

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 import {
     AlertDialog,
     AlertDialogAction,
@@ -8,7 +9,6 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
-    AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
 import {
@@ -17,35 +17,69 @@ import {
     InputOTPSeparator,
     InputOTPSlot,
 } from "@/components/ui/input-otp"
+import Image from 'next/image';
 
-const OTPModal = () => {
+const OTPModal = ({ accountid, email }: { accountId: string, email: string }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setisLoading(true);
+
+        try {
+
+        } catch (error) {
+            console.log("Failed to verify OTP", error);
+
+        }
+
+    };
+
+    const handleResetOtp = async () => {
+
+    }
     return (
-        <AlertDialog>
-            <AlertDialogTrigger>Open</AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+            <AlertDialogContent className='shad-alert-dialog'>
+                <AlertDialogHeader className='relative flex justify-center'>
+                    <AlertDialogTitle className='h2 text-center'>
+                        Enter your OTP
+                        <Image src="/assests/icons/close-dark.svg" alt='close' width={20} height={20} onClick={() => setIsOpen(false)} className='otp-close-button' />
+                    </AlertDialogTitle>
                     <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                        We&apos;ve sent a code to <span className='pl-1 text-brand'>{email}</span>
                     </AlertDialogDescription>
                 </AlertDialogHeader>
-                <InputOTP maxLength={6}>
-                    <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                        <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
-                    </InputOTPGroup>
-                    <InputOTPSeparator />
-                    <InputOTPGroup>
-                        <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
+                <InputOTP maxLength={6} value={password} onChange={setPassword}>
+                    <InputOTPGroup className='shad-otp'>
+                        <InputOTPSlot index={0} className='shad-otp-slot' />
+                        <InputOTPSlot index={1} className='shad-otp-slot' />
+                        <InputOTPSlot index={2} className='shad-otp-slot' />
+                        <InputOTPSlot index={3} className='shad-otp-slot' />
+                        <InputOTPSlot index={4} className='shad-otp-slot' />
+                        <InputOTPSlot index={5} className='shad-otp-slot' />
                     </InputOTPGroup>
                 </InputOTP>
                 <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction>Continue</AlertDialogAction>
+                    <div className="flex w-full flex-col gap-4">
+                        <AlertDialogAction
+                            onClick={handleSubmit}
+                            className='shad-submit-btn h-12'
+                            type='button'
+                        >
+                            Submit
+                            {isLoading && <Image 
+                                src="/assets/icons/loader.svg"
+                                alt='loader'
+                                width={24}
+                                height={24}
+                                className='ml-2 animate-spin'
+                            />}
+                        </AlertDialogAction>
+
+                    </div>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
