286872f9c5b55c1244233be577f5030b40b84094
Bektur13 <duyshembekov04@mail.ru>
2025-11-11 15:32:32 -0800

    Build SideBar component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,67 @@
+'use client';
+
+import { navItems } from '@/constants';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
-const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
+interface Props {
+    fullName: string,
+    avatar: string,
+    email: string
+}
+
+const SideBar = ({ fullName, avatar, email }: Props) => {
+
+    const pathname = usePathname();
+
+    return (
+        <aside className="sidebar">
+            <Link href='/'>
+                <Image src='/assets/icons/logo-full-brand.svg' alt='logo' height={50} width={160} className='hidden h-auto lg:block' />
+
+                <Image src='/assets/icons/logo-brand.svg' alt='logo' height={52} width={52} className='lg:hidden' />
+            </Link>
+
+            <nav className="sidebar-nav">
+                <ul className="flex">
+                    {navItems.map(({ url, name, icon }) => (
+                        <Link key={name} href={url} className="lg:w-full">
+                            <li
+                                className={cn(
+                                    "sidebar-nav-item",
+                                    pathname === url && "shad-active",
+                                )}
+                            >
+                                <Image
+                                    src={icon}
+                                    alt={name}
+                                    width={24}
+                                    height={24}
+                                    className={cn(
+                                        "nav-icon",
+                                        pathname === url && "nav-icon-active",
+                                    )}
+                                />
+                                <p className="hidden lg:block">{name}</p>
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </nav>
+
+            <Image src='/assets/images/files-2.png' alt='logo' width={506} height={418} className='w-full' />
+            <div className="sidebar-user-info">
+                <Image src={avatar} alt='Avatar' width={44} height={44} className='sidebar-user-info' />
+                <div className="hidden lg:block">
+                    <p className="subtitle-2 capitalize">{fullName}</p>
+                    <p className="caption">{email}</p>
+                </div>
+            </div>
+        </aside>
+    )
 }
 
 export default SideBar
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -143,24 +143,6 @@ export const signInUser = async ({email}: {email: string}) => {
     }
 }
 
-export const verifySecret = async ({ accountId, password}: {accountId: string; password:string}) => {
-    try {
-        const { account } = await createAdminClient();
-
-        const session = await account.createSession(accountId, password);
-
-        ((await cookies()).set('appwrite-session', session.secret, {
-            path: '/',
-            httpOnly: true,
-            sameSite: 'strict',
-            secure: true,
-        }));
-
-        return parseStringify({sessionId: session.$id});
-    } catch(error) {
-        handleError(error, 'Failed to verify OTP');
-    }
-}
 // import { Query } from "node-appwrite";
 // import { createAdminClient } from "../appwrite";
 // import { appwriteConfig } from "../appwrite/config";
