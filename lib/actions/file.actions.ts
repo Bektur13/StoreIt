99cdce7df34fab7c88e8e989e961eb5c78fdd715
Bektur13 <duyshembekov04@mail.ru>
2025-11-17 23:11:57 -0800

    Update file actions. Update utils.

---
+++ lib/actions/file.actions.ts
@@ -0,0 +1,95 @@
+"use server";
+
+import { createAdminClient } from "../appwrite";
+import { InputFile } from 'node-appwrite/file'
+import { appwriteConfig } from "../appwrite/config";
+import { constructFileUrl, getFileType, parseStringify } from "../utils";
+import { revalidatePath } from "next/cache";
+import { ID, Models, Query } from "node-appwrite";
+
+const handleError = (error: unknown, message: string) => {
+    console.log(error, message);
+    throw error;
+}
+
+export const uploadFiles = async ({
+    file,
+    ownerId,
+    accountId,
+    path
+}: UploadFileProps) => {
+    const { storage, databases } = await createAdminClient();
+
+    try {
+        const inputFile = InputFile.fromBuffer(file, file.name);
+
+        const bucketFile = await storage.createFile(
+            appwriteConfig.bucketId,
+            ID.unique(),
+            inputFile,
+        );
+
+        const fileDocument = {
+            type: getFileType(bucketFile.name).type,
+            name: bucketFile.name,
+            url: constructFileUrl(bucketFile.$id),
+            extension: getFileType(bucketFile.name).extension,
+            size: ownerId,
+            accountId,
+            users: [],
+            bucketFileId: bucketFile.$id,
+        };
+
+        const newFile = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            ID.unique(),
+            fileDocument,
+        ).catch(async (error: unknown) => {
+            await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+            handleError(error, "Failed to create file document");
+        });
+
+        revalidatePath(path);
+        return parseStringify(newFile);
+    } catch(error) {
+        handleError(error, "Failed to upload file")
+    }
+}
+
+const createQueries = (
+    currentUser: Models.Document,
+    types: string[],
+    searchText: string,
+    sort: string,
+    limit?: number,
+) => {
+    const queries = [
+        Query.or([
+            Query.equal("owner", [currentUser.$id]),
+            Query.contains("users", [currentUser.email])
+        ]),
+    ];
+
+    if(types.length > 0) queries.push(Query.equal("type", types));
+    if(searchText) queries.push(Query.contains("name", searchText));
+    if(limit) queries.push(Query.limit(limit));
+
+    if(sort) {
+        const [sortBy, orderBy] = sort.split("-");
+
+        queries.push(
+            orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+        )
+    }
+    return queries;
+}
+
+export const getFiles = async ({
+    types: [],
+    searchText = "",
+    sort = "$createdAt-desc",
+    limit,
+}: GetFilesProps) => {
+
+}
