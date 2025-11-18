f89667392eb1650029d0c5ee5d3781b285546dcb
Bektur13 <duyshembekov04@mail.ru>
2025-11-17 20:54:38 -0800

    Build new component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,9 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { Button } from './ui/button'
-import { cn } from '@/lib/utils'
+import { cn, convertFileToUrl } from '@/lib/utils'
 import Image from 'next/image'
+import Thumbnail from './Thumbnail'
 
 interface Props {
   ownerId: string,
@@ -11,6 +12,7 @@ interface Props {
 }
 
 const FileUploader = ({ownerId, accountId, className}: Props) => {
+  const [files, useFiles] = useState<File[]>([])
   
   const onDrop = useCallback(acceptedFiles => {
 
@@ -24,6 +26,20 @@ const FileUploader = ({ownerId, accountId, className}: Props) => {
         <Image src="/assets/icons/upload.svg" alt='upload' height={24} width={24}/>{" "}
         <p>Upload</p>
       </Button>
+      {files.length > 0 && <ul>
+          <h4 className="h4 text-light-100">Upload</h4>
+          {files.map((file,index) => {
+            const {type, extension} = getFileType(file.name);
+
+            return (
+              <li key={`${file.name}-${index}`} className='uploader-preview-item'>
+                <div className='flex items-center gap-3'>
+                  <Thumbnail type={extension} extension={extension} url={convertFileToUrl(file)}/>
+                </div>
+              </li>
+            )
+          })}
+        </ul>}
       {
         isDragActive ?
           <p>Drop the files here ...</p> :
