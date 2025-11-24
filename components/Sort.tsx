162acceb1a68d17b45d9333094b2ae8f4815979f
Bektur13 <duyshembekov04@mail.ru>
2025-11-23 22:54:08 -0800

    Add new component

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
     const path = usePathname();
@@ -11,7 +19,18 @@ const Sort = () => {
     };
 
     return (
-        <div></div>
+        <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+            <SelectTrigger className="sort-select">
+                <SelectValue placeholder={sortTypes[0].value} />
+            </SelectTrigger>
+            <SelectContent className="sort-select-content">
+                {sortTypes.map((sort) => (
+                    <SelectItem key={sort.label} className="shad-select-item" value={sort.value}>
+                        {sort.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     )
 };
 
